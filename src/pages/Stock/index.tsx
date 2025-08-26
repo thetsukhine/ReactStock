3c516319e84636e00685b185882cde0bad60e4f1
thetsukhine <thetsukhine.tsk@gmail.com>
2025-08-26 16:27:27 +0630

    update codes

--- src/pages/Stock/index.tsx
+++ src/pages/Stock/index.tsx
@@ -14,18 +14,59 @@ const data : { category: string, price: string, stocked: boolean, name: string  
 
 const Stock = () => {
 
+const [checked, setChecked] = useState(false);
+const [ search, setSearch  ] = useState("");
+
+const handleTextBoxChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+ const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);     
+  };
+
+    // Filter function: checks stock based on checkbox
+  const filterByStock = (item: typeof data[0]) => {
+    return checked ? item.stocked : true;
+  };
+
     return (
         <>
-        
+        <div className="padding-left-50">
+            <input  type="text" onChange={handleTextBoxChange}></input>
+        </div>
+           <div className="padding-left-50">
+      <label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+       Only show products in stock
+      </label>
+     
+    </div>
+
         <div>
-            <div>Name &nbsp; Price </div>
-            <div> Fruits  </div>
+            <div className="padding-left-100 font-bold">Name &nbsp; Price </div>
+            <br></br>
+            <div className="padding-left-100 font-bold"> Fruits  </div>
              {data
-             .filter ( (item) => item.category === "Fruits" ) 
+             .filter ( (item) => item.category === "Fruits" && filterByStock(item) ) 
              .map((item, index) => (
-        <div key={index} className={item.stocked ? "instock padding"  : "outofstock padding"}>  {item.name} &nbsp; {item.price}</div>
+        <div key={index} className={item.stocked ? "instock padding-left-50"  : "outofstock padding-left-50"}>  {item.name} &nbsp; {item.price}</div>
       ))}
-        </div></>
+
+            <br></br>
+
+        <div>
+             <div className="padding-left-100 font-bold"> Vegetables  </div>
+             {data
+             .filter ( (item) => item.category === "Vegetables" && filterByStock(item) ) 
+             .map((item, index) => (
+        <div key={index} className={item.stocked ? "instock padding-left-50"  : "outofstock padding-left-50"}>  {item.name} &nbsp; {item.price}</div>
+      ))}
+        </div>
+
+        </div>
+      
+        
+        
+        </>
     );
 }
 
